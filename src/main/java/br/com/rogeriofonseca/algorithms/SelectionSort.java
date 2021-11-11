69aeea357bd0441ed42708c2fc78f74a83cc8de3
Rogério Fonseca Gonçalves <rogerio.fonseca@gmail.com>
2021-11-10 22:17:26 -0300

    Implementing method that orders array... (SelectionSort)

--- src/main/java/br/com/rogeriofonseca/algorithms/SelectionSort.java
+++ src/main/java/br/com/rogeriofonseca/algorithms/SelectionSort.java
@@ -1,10 +1,12 @@
 package br.com.rogeriofonseca.algorithms;
 
+import java.util.Arrays;
+
 public class SelectionSort {
 
     public static int findSmallest(Products[] products, int begin, int end) {
 
-        int cheaperIndex = 0;
+        int cheaperIndex = begin;
         for(int i = begin; i <= end; i++) {
             if(products[cheaperIndex].getPrice() > products[i].getPrice()) {
                 cheaperIndex = i;
@@ -14,14 +16,20 @@ public class SelectionSort {
         return cheaperIndex;
     }
 
-    public static Products[] swapPositions(Products[] products, int positionA, int positionB) {
+    public static void swapPositions(Products[] products, int positionA, int positionB) {
 
         Products produtoA = products[positionA];
         Products produtoB = products[positionB];
         products[positionA]  = produtoB;
         products[positionB]  = produtoA;
+    }
 
-        return products;
+    public static Products[] sort(Products[] arrayInput) {
+        for(int i = 0; i < arrayInput.length; i++) {
+            int smallerIndex = findSmallest(arrayInput, i, arrayInput.length - 1);
+            swapPositions(arrayInput, i, smallerIndex);
+        }
+        return arrayInput;
     }
 
 }
--- src/test/java/br/com/rogeriofonseca/algorithms/SelectionSortTest.java
+++ src/test/java/br/com/rogeriofonseca/algorithms/SelectionSortTest.java
@@ -3,6 +3,9 @@ package br.com.rogeriofonseca.algorithms;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.text.MessageFormat;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -27,7 +30,7 @@ public class SelectionSortTest {
 
     @Test
     @DisplayName("Should swap positions")
-    void sortArray(){
+    void swapPostions(){
         Products[] products = {
             new Products("TV", 900.00),
             new Products("Notebook", 1200.00),
@@ -42,7 +45,37 @@ public class SelectionSortTest {
             new Products("Notebook", 1200.00), // HD Case - swaped
         };
 
-        Products[] listSwaped = SelectionSort.swapPositions(products, 1, 3);
-        assertArrayEquals(expectedProductsSwaped, listSwaped);
+        SelectionSort.swapPositions(products, 1, 3);
+        assertArrayEquals(expectedProductsSwaped, products);
+    }
+
+    @Test
+    @DisplayName("Should sort array")
+    void sortArray(){
+        Products[] arrayInput = {
+            new Products("TV", 900.00),
+            new Products("Notebook", 1200.00),
+            new Products("Tablet", 450.00),
+            new Products("HD Case", 80.90),
+        };
+
+        Products[] arrayExpected = {
+            new Products("HD Case", 80.90), // Notebook - swaped
+            new Products("Tablet", 450.00),
+            new Products("TV", 900.00),
+            new Products("Notebook", 1200.00), // HD Case - swaped
+        };
+
+        Products[] arraySorted = SelectionSort.sort(arrayInput);
+
+        // Print array sorted
+        // System.out.println("Array sorted: ");
+        // Arrays.stream(arraySorted)
+        //     .forEach(e-> {
+        //         String message = MessageFormat.format("Price: {0}, Model: {1}", e.getPrice(), e.getModel());
+        //         System.out.println(message);
+        //     });
+
+        assertArrayEquals(arrayExpected, arraySorted);
     }
 }
